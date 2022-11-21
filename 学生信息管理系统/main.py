62f241ddd77f5fca1f989b6e78e4fb6c715225e3
github-czy <1060324818@qq.com>
2022-11-21 15:57:05 +0800

    学生信息管理系统项目全部代码，项目完结

--- 学生信息管理系统/main.py
+++ 学生信息管理系统/main.py
@@ -3,9 +3,9 @@
 # file main.py
 # outhor:czy
 # email:1060324818@qq.com
+from datetime import timedelta
 
-
-from flask import Flask,render_template,request,redirect
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 app = Flask(__name__)
 
@@ -15,26 +15,42 @@ students = [{ 'name': '张三','chinese': '65','math': '65','english': '65'},
             { 'name' : '赵六', 'chinese': '65', 'math': '65','english': '65'}]
 a = len(students)
 
+app.secret_key = "czy"
+#半永久会话,保持五天,也可以改成minutes=5，五分钟
+app.permanent_session_lifetime = timedelta(days=5)
+
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return "<p>hello!</p>"
+    return render_template("home.html")
 
 @app.route('/login',methods=["POST","GET"])
 def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        return redirect('/admin')
+
+        session["username"] = username
+        session["password"] = password
+        # 半永久会话
+        session.permanent = True
+        # 显示已经登录成功
+        flash("login successful!", "info")
+        return redirect('/home')
     return render_template("login.html")
 
 @app.route('/admin')
 def admin():
-    return render_template("admin.html",students=students,a = a)
+    if "username" in session:
+        return render_template("admin.html",students=students,a = a)
+    else:
+        flash("you are not login in!", "info")
+        return render_template("login.html")
 
 @app.route('/add',methods=["POST","GET"])
 def add():
-    if request.method == "POST":
+    if request.method == "POST" :
         username = request.form["username"]
         chinese = request.form["chinese"]
         math = request.form["math"]
@@ -47,9 +63,9 @@ def add():
         dicts["english"] = english
 
         students.append(dicts)
-
         return redirect("/admin")
     return render_template("add.html")
+
 
 @app.route('/delete')
 def delete_student():
@@ -61,7 +77,7 @@ def delete_student():
 
 @app.route('/change',methods=["POST","GET"])
 def change_student():
-    if request.method == "POST":
+    if request.method == "POST" and "username" in session:
         chage_name = request.form['username']
         for i in students:
             if i['name'] == chage_name:
@@ -75,11 +91,39 @@ def change_student():
                 i["math"] = math
                 i["english"] = english
         return redirect("/admin")
-    else:
+    elif request.method == "GET" and "username" in session:
         change_name = request.args['name']
         for i in students:
             if i['name'] == change_name:
                 return render_template("chage.html", students=i)
+    else:
+        flash("you are not login in!", "info")
+        return redirect("/login")
+
+
+#清除会话，退出登录
+@app.route("/logout")
+def logout():
+    # 页面间消息的传递
+    flash(f"you have beign logged out！", "info")
+    session.pop("username",None)
+    session.pop("password", None)
+    return redirect(url_for("login"))
+
+#数据可视化
+@app.route("/view")
+def view():
+    dicts = [0,0,0,0]
+    for i in students:
+        if int(i["chinese"]) < 60:
+            dicts[3] += 1
+        elif int(i["chinese"]) >= 60 and int(i["chinese"]) < 70:
+            dicts[0] += 1
+        elif int(i["chinese"]) >= 70 and int(i["chinese"]) < 90:
+            dicts[1] += 1
+        elif int(i["chinese"]) >= 90 and int(i["chinese"]) <= 100:
+            dicts[2] += 1
+    return render_template("view.html",stu=dicts)
 
 if __name__ == '__main__':
     app.run(debug=True)
